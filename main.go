a75bc7e9fd339d7c15242b9d4201cfac51cb8272
stevenpdevries <77169672+stevenpdevries@users.noreply.github.com>
2021-09-02 15:37:24 -0500

    Update main.go

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 		}
 		sort.Strings(res.EnvVars)
 
-		res.Message2 = "Inside main() of Go program inside the container image on Wednesday 9/1"
+		res.Message2 = "Inside main() of Go program inside the container image on Thursday 9/2"
 
 		// Beautify the JSON output
 		out, _ := json.MarshalIndent(res, "", "  ")
